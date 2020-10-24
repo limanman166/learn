b2a91ef24eaaaa66ccb325431f7fb1ffaea13cef
liman <1752199902@qq.com>
2020-10-24 22:13:57 +0800

    1233

---
+++ test.py
@@ -0,0 +1,11 @@
+class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('i am run')
+
+    def jump(self):
+        print('i can jump')
